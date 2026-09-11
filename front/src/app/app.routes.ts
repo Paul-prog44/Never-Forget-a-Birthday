ab97e5d0d429199c8da7dca1cdaacb531d2c5c55
Gerald <saintouenfr@hotmail.com>
2026-09-11 11:02:01 +0200

    profile component added

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './features/home/home';
 import { Register } from './features/auth/register/register';
 import { Login } from './features/auth/login/login';
+import { Profile } from './features/profile/profile';
 
 export const routes: Routes = [
     {path:'', component: HomeComponent},
     {path: 'register', component: Register},
-    {path: 'login', component: Login}
+    {path: 'login', component: Login},
+    {path: 'profile', component: Profile}
 ];
